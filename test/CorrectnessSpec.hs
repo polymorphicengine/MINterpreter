5d3fe417d0832473344700e417f0b8544de67b31
onthepeakofnormal <gius_martin@yahoo.de>
2021-05-17 16:58:29 +0200

    add automatic testing for correctness of mini programs

--- test/CorrectnessSpec.hs
+++ test/CorrectnessSpec.hs
@@ -1,6 +1,6 @@
 module CorrectnessSpec where
 
-  import Correctness(prog1_corr)
+  import Correctness(prog1_corr,prog2_corr,prog3_corr)
   import Test.Hspec
   import Test.Hspec.QuickCheck
 
@@ -8,4 +8,7 @@ module CorrectnessSpec where
   main = hspec spec
 
   spec :: Spec
-  spec = do prop "checks the correctness of the fibonacci program" $ prog1_corr
+  spec = do
+    prop "checks the correctness of the fibonacci program" $ prog1_corr
+    prop "checks the correctness of the prime number check" $ prog2_corr
+    prop "checks the correctness of the lcm program" $ prog3_corr
