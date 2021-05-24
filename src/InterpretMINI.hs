0f5effaf4555b636830ea8a49eae8c54afaee596
onthepeakofnormal <gius_martin@yahoo.de>
2021-05-24 22:36:25 +0200

    add comments

--- src/InterpretMINI.hs
+++ src/InterpretMINI.hs
@@ -1,18 +1,25 @@
 module InterpretMINI where
 
+{-| ------------------------
+            import
+-}  ------------------------
+
 import ParseMINI
 
 import Control.Monad.Identity
 import Control.Monad.State
-
 import Data.Maybe
 import qualified Data.Map as Map
 
 type Value = Integer
-type Env = Map.Map Name Value
+type Env = Map.Map Name Value -- mapping from names to values
+type StateID = StateT Env Identity -- handling states
 
-type StateID = StateT Env Identity
+{-| ---------------------------------------
+        well-formed-expression interpreter
+-}  ---------------------------------------
 
+-- nested expression
 expNestEval:: ExpressionNested -> StateID Value
 expNestEval (ENum i) = return i
 expNestEval (EVar (Var name)) = do
@@ -22,6 +29,7 @@ expNestEval (EVar (Var name)) = do
                                     (Just val) -> return val
 expNestEval (Exp expr) = expEval expr
 
+-- combined expression
 expEval :: Expression -> StateID Value
 expEval (Pos expr) = expNestEval expr
 expEval (Neg expr) =  do
@@ -36,6 +44,7 @@ expEval (Term exp1 op exp2) = do
                               getOp Times = (*)
                               getOp Divide = (div)
 
+-- boolean expression
 boolEval :: Boolean -> StateID Bool
 boolEval (BExp exp1 rel exp2) = do
                       num1 <- expEval exp1
@@ -48,12 +57,18 @@ boolEval (BExp exp1 rel exp2) = do
                             getRel LE = (<)
                             g1etRel GE = (>)
 
+{-| ---------------------------------------
+            statement interpreter
+-}  ---------------------------------------
+
+-- variable assignment
 assignEval :: Assign -> StateID ()
 assignEval (Ass (Var name) expr) = do
                             env <- get
                             num <- expEval expr
                             put (Map.insert name num env)
 
+-- if-or-ifelse statement
 ifEval:: If -> StateID ()
 ifEval (If boolExp stats) = do
                       bool <- boolEval boolExp
@@ -68,6 +83,7 @@ ifEval (Elif boolExp stats1 stats2) = do
                       else
                         statsEval stats2
 
+-- while statement
 whileEval:: While -> StateID ()
 whileEval w@(While boolExp stats) = do
                       bool <- boolEval boolExp
@@ -78,17 +94,24 @@ whileEval w@(While boolExp stats) = do
                       else
                         return ()
 
+-- statement
 statEval:: Statement -> StateID ()
 statEval (WSt w) = whileEval w
 statEval (ISt i) = ifEval i
 statEval (ASt a) = assignEval a
 
+-- statements
 statsEval :: Statements -> StateID ()
 statsEval Eps = return ()
 statsEval (St stat stats) = do
                         statEval stat
                         statsEval stats
 
+{-| --------------------------
+            program
+-}  --------------------------
+
+-- return statement
 returnEval :: Return -> StateID Value
 returnEval (Return (Var name)) = do
                           env <- get
@@ -96,36 +119,44 @@ returnEval (Return (Var name)) = do
                                 Nothing -> error "undefined variable call"
                                 (Just val) -> return val
 
+-- procedure statement
 procedureEval:: Procedure -> StateID Value
 procedureEval (Proc stats ret) = do
                               statsEval stats
                               returnEval ret
 
-argsToList:: Arguments -> [Var]
-argsToList (Arg v) = [v]
-argsToList (Args v vs) = v:(argsToList vs)
-
--- We can throw an error here if the amount of arguments is not equal to the amount if inputs
+-- procedure arguments
 argumentEval:: Arguments -> [Integer] -> Env
 argumentEval args input | length input /= length argList = error "length of input does not match length of arguments"
                         | otherwise = Map.unions $ zipWith (\(Var name) i -> Map.insert name i Map.empty) argList input
                       where argList = argsToList args
 
+-- program
 runProgram :: [Integer] -> Program -> Value
 runProgram inputs (Prog args procedure) = genRun procedureEval procedure (argumentEval args inputs)
 
 
+{-| --------------------------
+       auxiliary functions
+-}  --------------------------
+
+-- procedure arguments
+argsToList:: Arguments -> [Var]
+argsToList (Arg v) = [v]
+argsToList (Args v vs) = v:(argsToList vs)
+
 genRun :: (b -> StateID a) -> b -> Env -> a
 genRun eval input env = runIdentity $ evalStateT (eval input) env
 
--- TESTS
 
+
+-- T O  D E L E T E
+
+-- TESTS
 
 -- runIdentity $(evalStateT $ evalExp Map.empty (Pos (ENum 5))) Map.empty
 -- runIdentity $(evalStateT $ evalExp Map.empty ( (Term (Exp (Term (ENum 1) Plus (ENum 2))) Times (ENum 3)) )) Map.empty
 -- runIdentity $ runStateT  (assignEval (Ass (Var "x") (Pos (ENum 5)))) Map.empty
-
-
 
 ---(St (ASt (Ass (Var "c") (Term (ENum 5) Minus (ENum 2)))) (St (ASt (Ass (Var "d") (Pos (EVar (Var "c"))))) Eps))
 -- runIdentity $ runStateT  (statsEval (St (ASt (Ass (Var "c") (Term (ENum 5) Minus (ENum 2)))) (St (ASt (Ass (Var "d") (Pos (EVar (Var "c"))))) Eps))) Map.empty
@@ -143,3 +174,15 @@ genRun eval input env = runIdentity $ evalStateT (eval input) env
 -- greatest common divisor example
 --(Prog (Args (Var "a") (Arg (Var "b"))) (Proc (St (ASt (Ass (Var "r") (Pos (EVar (Var "b"))))) (St (ISt (If (BExp (Pos (EVar (Var "a"))) NEQ (Pos (ENum 0))) (St (WSt (While (BExp (Pos (EVar (Var "b"))) NEQ (Pos (ENum 0))) (St (ISt (Elif (BExp (Pos (EVar (Var "a"))) LEQ (Pos (EVar (Var "b")))) (St (ASt (Ass (Var "b") (Term (EVar (Var "b")) Minus (EVar (Var "a"))))) Eps) (St (ASt (Ass (Var "a") (Term (EVar (Var "a")) Minus (EVar (Var "b"))))) Eps))) Eps))) (St (ASt (Ass (Var "r") (Pos (EVar (Var "a"))))) Eps)))) Eps)) (Return (Var "r"))))
 -- runProgram [42,56] (Prog (Args (Var "a") (Arg (Var "b"))) (Proc (St (ASt (Ass (Var "r") (Pos (EVar (Var "b"))))) (St (ISt (If (BExp (Pos (EVar (Var "a"))) NEQ (Pos (ENum 0))) (St (WSt (While (BExp (Pos (EVar (Var "b"))) NEQ (Pos (ENum 0))) (St (ISt (Elif (BExp (Pos (EVar (Var "a"))) LEQ (Pos (EVar (Var "b")))) (St (ASt (Ass (Var "b") (Term (EVar (Var "b")) Minus (EVar (Var "a"))))) Eps) (St (ASt (Ass (Var "a") (Term (EVar (Var "a")) Minus (EVar (Var "b"))))) Eps))) Eps))) (St (ASt (Ass (Var "r") (Pos (EVar (Var "a"))))) Eps)))) Eps)) (Return (Var "r"))))
+
+
+-- N E W   progs
+
+-- factorial of n
+-- runProgram [10] (Prog (Arg (Var "n")) (Proc (St (ASt (Ass (Var "i") (Pos (ENum 1)))) (St (ASt (Ass (Var "fac") (Pos (ENum 1)))) (St (ISt (Elif (BExp (Pos (EVar (Var "n"))) LE (Pos (ENum 0))) (St (ASt (Ass (Var "fac") (Pos (ENum 0)))) Eps) (St (WSt (While (BExp (Pos (EVar (Var "n"))) GEQ (Pos (EVar (Var "i")))) (St (ASt (Ass (Var "fac") (Term (EVar (Var "fac")) Times (EVar (Var "i"))))) (St (ASt (Ass (Var "i") (Term (EVar (Var "i")) Plus (ENum 1)))) Eps)))) Eps))) Eps))) (Return (Var "fac"))))
+
+-- reversal of n
+-- runProgram [123456789] (Prog (Arg (Var "n")) (Proc (St (ASt (Ass (Var "reverse") (Pos (ENum 0)))) (St (WSt (While (BExp (Pos (EVar (Var "n"))) NEQ (Pos (ENum 0))) (St (ASt (Ass (Var "rem") (Term (EVar (Var "n")) Minus (Exp (Term (ENum 10) Times (Exp (Term (EVar (Var "n")) Divide (ENum 10)))))))) (St (ASt (Ass (Var "reverse") (Pos (Exp (Term (Exp (Term (EVar (Var "reverse")) Times (ENum 10))) Plus (EVar (Var "rem"))))))) (St (ASt (Ass (Var "n") (Term (EVar (Var "n")) Divide (ENum 10)))) Eps))))) Eps)) (Return (Var "reverse"))))
+
+-- count digits of n
+-- runProgram [71283719823791273] (Prog (Arg (Var "n")) (Proc (St (ASt (Ass (Var "counter") (Pos (ENum 0)))) (St (WSt (While (BExp (Pos (EVar (Var "n"))) NEQ (Pos (ENum 0))) (St (ASt (Ass (Var "n") (Term (EVar (Var "n")) Divide (ENum 10)))) (St (ASt (Ass (Var "counter") (Term (EVar (Var "counter")) Plus (ENum 1)))) Eps)))) Eps)) (Return (Var "counter"))))
--- src/ParseMINI.hs
+++ src/ParseMINI.hs
@@ -1,5 +1,9 @@
 module ParseMINI where
 
+{-| ------------------------
+            import
+-}  ------------------------
+
 import Text.Parsec.String(Parser)
 import Text.Parsec(many, satisfy, try, (<|>), parse)
 import Text.Parsec.Char(char, oneOf, digit, string)
@@ -7,21 +11,25 @@ import Text.Parsec.Combinator(choice, eof, between, many1)
 import Text.Parsec.Error(ParseError)
 import Data.Char (isLetter, isDigit)
 import Control.Monad(void)
--- import Control.Applicative ((<|>))
 
---Terminals
+{-| ------------------------
+           terminals
+-}  ------------------------
 
 data Relator = EQQ | NEQ | LEQ | GEQ | LE | GE deriving (Show, Eq)
 data Operator = Plus | Minus | Times | Divide deriving (Show, Eq)
 -- data Digit = Zero | One | Two | Three | Four | Five | Six | Seven | Eight | Nine deriving (Show, Eq, Enum)
 -- data Letter = A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z deriving (Show, Eq)
 
---Non-Terminals
+{-| ------------------------
+          non-Terminals
+-}  ------------------------
 
 -- data Ident_Char = Let Letter | Dig Int | Score deriving (Show, Eq)
 -- data Ident_Rest = Eps | Rest Ident_Char Ident_Rest deriving (Show, Eq)
 -- data Ident = Ident Letter Ident_Rest deriving (Show, Eq)
 -- newtype Ident = Ident String deriving (Show, Eq)
+
 type Name = String
 newtype Var = Var Name deriving (Show, Eq)
 -- data Number = Digit Digit | Number Digit Number deriving (Show, Eq)
@@ -38,57 +46,48 @@ data Procedure = Proc Statements Return deriving (Show, Eq)
 data Arguments = Arg Var | Args Var Arguments deriving (Show, Eq)
 data Program = Prog Arguments Procedure deriving (Show, Eq)
 
---helper functions
+{-| --------------------------
+       auxiliary functions
+-}  --------------------------
 
+isSmallLetter :: Char -> Bool
+isSmallLetter c = elem c "abcdefghijklmnopqrstuvwxyz"
+
+isOperator:: Char -> Bool
+isOperator c = elem c "+-*/"
+
+--  parse (>= 1) ASCII digits, return parsed characters
+num :: Parser Integer
+num = do
+    n <- many1 digit
+    return (read n)
+
+-- consuming whitespace
 whitespace :: Parser ()
 whitespace = void $ many $ oneOf " \n\t"
 
+-- consume trailing whitespace
 lexeme :: Parser a -> Parser a
 lexeme p = do
            x <- p
            whitespace
            return x
 
-isSmallLetter :: Char -> Bool
-isSmallLetter c = elem c "abcdefghijklmnopqrstuvwxyz"
+-- parse single character, return parsed character
+symbol :: Char -> Parser Char
+symbol c = lexeme $ char c
 
-isOperator:: Char -> Bool
-isOperator c = elem c "+-/*"
+-- parse "(", parse ")", return enclosed characters
+betweenParens :: Parser a -> Parser a
+betweenParens p = between (symbol '(') (symbol ')') p
 
-num :: Parser Integer
-num = do
-    n <- many1 digit
-    return (read n)
+-- parse "{", parse "}", return enclosed characters
+betweenParensCurly :: Parser a -> Parser a
+betweenParensCurly p = between (symbol '{') (symbol '}') p
 
-numE :: Parser ExpressionNested
-numE = do
-  x <- lexeme num
-  return $ ENum x
-
-variable :: Parser Var
-variable = do
-    fc <- firstChar
-    rest <- many nonFirstChar
-    return $ Var (fc:rest)
-  where
-    firstChar = satisfy (\a -> isSmallLetter a)
-    nonFirstChar = satisfy (\a -> isDigit a || isSmallLetter a || a == '_')
-
-varE :: Parser ExpressionNested
-varE = do
-  x <- lexeme variable
-  return $ EVar x
-
-expPos :: Parser Expression
-expPos = do
-      x <- lexeme expNestParse
-      return $ Pos x
-
-expNeg :: Parser Expression
-expNeg = do
-      fc <- char '-'
-      x <- lexeme expNestParse
-      return $ Neg x
+{-| --------------------------
+         symbol parsers
+-}  --------------------------
 
 operatorParse :: Parser Operator
 operatorParse = do
@@ -99,6 +98,76 @@ operatorParse = do
                  chooseOp '*' = Times
                  chooseOp '/' = Divide
 
+geqP :: Parser Relator
+geqP = do
+     x <- string ">="
+     return GEQ
+
+leqP :: Parser Relator
+leqP = do
+     x <- string "<="
+     return LEQ
+
+eqP :: Parser Relator
+eqP = do
+     x <- string "=="
+     return EQQ
+
+neqP :: Parser Relator
+neqP = do
+     x <- string "!="
+     return NEQ
+
+leP :: Parser Relator
+leP = do
+     x <- string "<"
+     return LE
+
+geP :: Parser Relator
+geP = do
+     x <- string ">"
+     return GE
+
+{-| --------------------------------------
+         well-formed-formula parsers
+-}  --------------------------------------
+
+-- numerical expression
+numE :: Parser ExpressionNested
+numE = do
+  x <- lexeme num
+  return $ ENum x
+
+-- variable expression
+variable :: Parser Var
+variable = do
+    fc <- firstChar
+    rest <- many nonFirstChar
+    return $ Var (fc:rest)
+  where
+    firstChar = satisfy (\a -> isSmallLetter a)
+    nonFirstChar = satisfy (\a -> isDigit a || isSmallLetter a || a == '_')
+
+-- nested variable expression
+varE :: Parser ExpressionNested
+varE = do
+  x <- lexeme variable
+  return $ EVar x
+
+-- postives expression
+expPos :: Parser Expression
+expPos = do
+      x <- lexeme expNestParse
+      return $ Pos x
+
+-- negative expression
+expNeg :: Parser Expression
+expNeg = do
+      fc <- char '-'
+      x <- lexeme expNestParse
+      return $ Neg x
+
+-- combined expression
 expTerm:: Parser Expression
 expTerm = do
       x <- lexeme expNestParse
@@ -106,12 +175,28 @@ expTerm = do
       y <- lexeme expNestParse
       return $ Term x o y
 
+-- braced expression
 bracesE :: Parser ExpressionNested
 bracesE = do
-    void $ lexeme $ char '('
-    e <- expParse
-    void $ lexeme $ char ')'
-    return $ Exp e
+      void $ lexeme $ char '('
+      e <- expParse
+      void $ lexeme $ char ')'
+      return $ Exp e
+
+-- boolean expression
+booleanParse :: Parser Boolean
+booleanParse = do
+            x <- lexeme expParse
+            r <- lexeme relatorParse
+            y <- lexeme expParse
+            return $ BExp x r y
+
+-- combination parsers (well-formed-formula)
+expNestParse :: Parser ExpressionNested
+expNestParse = lexeme bracesE <|> lexeme varE <|> lexeme numE
+
+relatorParse :: Parser Relator
+relatorParse = try geqP <|> try leqP <|> eqP <|> neqP <|> leP <|> geP
 
 expParse :: Parser Expression
 expParse = choice [try $ lexeme expTerm, lexeme expNeg, lexeme expPos]
@@ -123,50 +208,11 @@ expParseWithEOF = do
                 eof
                 return x
 
-expNestParse :: Parser ExpressionNested
-expNestParse = lexeme bracesE <|> lexeme varE <|> lexeme numE
+{-| --------------------------
+           statements
+-}  --------------------------
 
-
-geqP :: Parser Relator
-geqP = do
-      x <- string ">="
-      return GEQ
-
-leqP :: Parser Relator
-leqP = do
-      x <- string "<="
-      return LEQ
-
-eqP :: Parser Relator
-eqP = do
-      x <- string "=="
-      return EQQ
-
-neqP :: Parser Relator
-neqP = do
-  x <- string "!="
-  return NEQ
-
-leP :: Parser Relator
-leP = do
-  x <- string "<"
-  return LE
-
-geP :: Parser Relator
-geP = do
-  x <- string ">"
-  return GE
-
-relatorParse :: Parser Relator
-relatorParse = try geqP <|> try leqP <|> eqP <|> neqP <|> leP <|> geP
-
-booleanParse :: Parser Boolean
-booleanParse = do
-            x <- lexeme expParse
-            r <- lexeme relatorParse
-            y <- lexeme expParse
-            return $ BExp x r y
-
+-- variable assignment
 assignParse :: Parser Assign
 assignParse = do
             x <- lexeme $ variable
@@ -175,6 +221,7 @@ assignParse = do
             semi <- lexeme $ char ';'
             return $ Ass x z
 
+-- if statement
 ifParse' :: Parser If
 ifParse' = do
       x <- lexeme $ string "if"
@@ -182,6 +229,7 @@ ifParse' = do
       z <- lexeme $ betweenParensCurly statementsParse
       return $ If y z
 
+-- ifelse statement
 elifParse :: Parser If
 elifParse = do
       x <- lexeme $ string "if"
@@ -191,9 +239,11 @@ elifParse = do
       elS <- lexeme $ betweenParensCurly statementsParse
       return $ Elif y z elS
 
+-- if-or-ifelse statement
 ifParse :: Parser If
 ifParse = try elifParse <|> ifParse'
 
+-- while statement
 whileParse :: Parser While
 whileParse = do
       x <- lexeme $ string "while"
@@ -201,6 +251,15 @@ whileParse = do
       z <- lexeme $ betweenParensCurly statementsParse
       return $ While y z
 
+-- empty statement
+emptyParse :: Parser Statements
+emptyParse = return Eps
+
+-- statement
+statementParse :: Parser Statement
+statementParse = (try $ fmap WSt whileParse) <|> (try $ fmap ISt ifParse) <|> fmap ASt assignParse
+
+-- statements
 statementsParseRec :: Parser Statements
 statementsParseRec = do
             x <- lexeme statementParse
@@ -210,21 +269,11 @@ statementsParseRec = do
 statementsParse :: Parser Statements
 statementsParse = try statementsParseRec <|> emptyParse
 
-statementParse :: Parser Statement
-statementParse = (try $ fmap WSt whileParse) <|> (try $ fmap ISt ifParse) <|> fmap ASt assignParse
+{-| --------------------------
+            program
+-}  --------------------------
 
-symbol :: Char -> Parser Char
-symbol c = lexeme $ char c
-
-betweenParens :: Parser a -> Parser a
-betweenParens p = between (symbol '(') (symbol ')') p
-
-betweenParensCurly :: Parser a -> Parser a
-betweenParensCurly p = between (symbol '{') (symbol '}') p
-
-emptyParse :: Parser Statements
-emptyParse = return Eps
-
+-- return statement
 returnParse :: Parser Return
 returnParse = do
       x <- lexeme $ string "return"
@@ -232,6 +281,14 @@ returnParse = do
       semi <- lexeme $ char ';'
       return $ Return v
 
+-- procedure statement
+procParse :: Parser Procedure
+procParse = do
+      stat <- lexeme $ statementsParse
+      ret <- lexeme $ returnParse
+      return $ Proc stat ret
+
+-- procedure arguments
 argVarParseRec :: Parser Arguments
 argVarParseRec = do
             x <- lexeme $ variable
@@ -242,12 +299,7 @@ argVarParseRec = do
 argVarParse :: Parser Arguments
 argVarParse = try argVarParseRec <|> (fmap Arg variable)
 
-procParse :: Parser Procedure
-procParse = do
-      stat <- lexeme $ statementsParse
-      ret <- lexeme $ returnParse
-      return $ Proc stat ret
-
+-- program
 programParse :: Parser Program
 programParse = do
       key <- lexeme $ string "procedure main"
@@ -262,18 +314,13 @@ programParseEOF = do
         eof
         return prog
 
-catch:: Either ParseError a -> a
-catch (Left err) = error (show err)
-catch (Right expr) = expr
 
 
 
---Test: "1 +(3 + 4)"
+-- Verwenden wir nicht
+-- catch:: Either ParseError a -> a
+-- catch (Left err) = error (show err)
+-- catch (Right expr) = expr
 
---- exp := var | int | -var | -int | (-exp) | (exp op exp)
-
--- lookAhead $ satisfy (\a -> a == '(') <|>
-
--- "procedure main ( a , b ) {c = a - b; return c ; }"
--- "procedure main ( a , b ) {if (a <  b) {c =  a  ;} else    {c =b  ;  }   return c ;}"
--- procedure main (a , b) {r = b ; if  ( a != 0) {while (b != 0) { if ( a <= b) {b = b - a ;} else {a = a - b ;} } r = a ; } return r ;}
+-- F R A G E
+-- expParseWithEOF einsetzen?
